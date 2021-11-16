ecc31a6be3c787feab8b3d2784a1e8231fe91eae
isaac <xuhg@dpark.com.cn>
2021-11-16 17:43:52 +0800

    ....

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <div id="app">
-
+    hello world
   </div>
 </template>
 
 <script>
-import xdoBrowerFilter from './packages/xdo-browser-filter/index.js'
+import xdoBrowserFilter from './packages/xdo-browser-filter/index.js'
 export default {
   name: 'App',
   components: {
   },
   created() {
-    xdoBrowerFilter({
+    xdoBrowserFilter({
       filter:{
-        chrome:65,
+        chrome:100,
         ie:10,
         firefox:53
       },
+      dialogType:'modal',
       // 兼容的浏览器下载链接
       newVerUrl: 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B8E74F7DE-458E-65DD-EC25-16AB30074375%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe',
       // 下载的文件名称
--- src/packages/xdo-browser-filter/index.js
+++ src/packages/xdo-browser-filter/index.js
@@ -4,13 +4,13 @@ function getBrowserInfo(){
     let sys = {};
     let ua = navigator.userAgent.toLowerCase();
     let s;
-    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys = {browser: 'ie', ver: s[1]}
-        : (s = ua.match(/msie ([\d\.]+)/)) ? sys = {browser: 'ie', ver: s[1]}
-            : (s = ua.match(/edge\/([\d\.]+)/)) ? sys = {browser: 'edge', ver: s[1]}
-                : (s = ua.match(/firefox\/([\d\.]+)/)) ? sys = {browser: 'firefox', ver: s[1]}
-                    : (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? sys = {browser: 'opera', ver: s[1]}
-                        : (s = ua.match(/chrome\/([\d\.]+)/)) ? sys = {browser: 'chrome', ver: s[1]}
-                            : (s = ua.match(/version\/([\d\.]+).*safari/)) ? sys = {browser: 'safari', ver: s[1]} : 0
+    (s = ua.match(/rv:([\d\\.]+)\) like gecko/)) ? sys = {browser: 'ie', ver: s[1]}
+        : (s = ua.match(/msie ([\d\\.]+)/)) ? sys = {browser: 'ie', ver: s[1]}
+            : (s = ua.match(/edge\/([\d\\.]+)/)) ? sys = {browser: 'edge', ver: s[1]}
+                : (s = ua.match(/firefox\/([\d\\.]+)/)) ? sys = {browser: 'firefox', ver: s[1]}
+                    : (s = ua.match(/(?:opera|opr).([\d\\.]+)/)) ? sys = {browser: 'opera', ver: s[1]}
+                        : (s = ua.match(/chrome\/([\d\\.]+)/)) ? sys = {browser: 'chrome', ver: s[1]}
+                            : (s = ua.match(/version\/([\d\\.]+).*safari/)) ? sys = {browser: 'safari', ver: s[1]} : 0
     return sys;
 }
 
@@ -20,7 +20,7 @@ function createMongolianLayer(options){
     mongolianLayer.innerHTML='<div class="bf-container">' +
         '<div class="bf-dialog">' +
         '<div class="bf-imgContainer">' +
-        '<a href="'+options.newVerUrl+'"  class="bf-imgIcon '+options.class+'" download="'+options.downloadName+'"></a>' +
+        '<a href="'+options.newVerUrl+'"  class="bf-imgIcon '+options.class+'" download="'+options.title+'"></a>' +
         '</div>' +
         '<p class="bf-description">' +
         options.title+
@@ -36,15 +36,11 @@ function init(options){
         },
         title:'您的页面似乎有兼容性问题，点击图标下载最新版Chrome！',
         class:'',
-        dialogType:'fullScreen',
+        dialogType:'full',
         newVerUrl: 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B8E74F7DE-458E-65DD-EC25-16AB30074375%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe',
         downloadName:'ChromeSetup.exe'
     };
-    _options.filter=options.filter||_options.filter;
-    _options.downloadName=options.title||_options.title;
-    _options.downloadName=options.class||_options.class;
-    _options.newVerUrl=options.newVerUrl||_options.newVerUrl;
-    _options.downloadName=options.downloadName||_options.downloadName;
+    _options = Object.assign(_options,options)
     let browserInfo = getBrowserInfo();
     if(_options.filter[browserInfo.browser]){
         if(browserInfo.ver){
@@ -53,6 +49,9 @@ function init(options){
                 switch(_options.dialogType){
                     case "fullScreen":{
                         createMongolianLayer(_options);
+                    }break;
+                    case "modal":{
+                        alert('xxxx')
                     }break;
                     default:{
                         createMongolianLayer(_options);
