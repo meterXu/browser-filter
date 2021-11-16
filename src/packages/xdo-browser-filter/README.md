bc0d2c8bb4214e62d41b42b6924ab0f5ae76dbc8
isaac <772413635@qq.com>
2021-11-16 21:13:23 +0800

    ....

--- src/packages/xdo-browser-filter/README.md
+++ src/packages/xdo-browser-filter/README.md
@@ -18,27 +18,22 @@
 ```
 ## 用法
 ```javascript
-window.onload=function () {
-    xdoBrowserFilter({
-                filter:{
-                    chrome:65, // 限定页面支持的浏览器最小版本
-                    ie:10,
-                    firefox:53
-                },
-                // 兼容的浏览器下载链接
-                newVerUrl: 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B8E74F7DE-458E-65DD-EC25-16AB30074375%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe',
-                // 下载的文件名称
-                downloadName:'ChromeSetup.exe'
-            });
-        };
+xdoBrowserFilter({
+    filter:{
+        chrome:65,
+        ie:10,
+        firefox:53
+    },
+    action:function(res){
+        console.log(res)
+    }
+})
 ```
 ## API
 ### options
 |  参数   | 类型  |  默认值  |  描述  |
 |  ----  | ----  | ----  | ----  |
-| filter  | Object | {} |限定页面兼容的浏览器最小版本|
-| title  | String | 您的页面似乎有兼容性问题，点击图标下载最新版Chrome！ |提示文字|
-| class  | String | [空字符串] |图标样式|
-| newVerUrl  | String | [chrome下载地址](https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B8E74F7DE-458E-65DD-EC25-16AB30074375%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe) |浏览器下载地址|
-| downloadName  | String | ChromeSetup.exe |下载资源名称|
-|dialogType|String| full | 不兼容时的提示,可选full、modal、notice |
+| filter  | Object | {} |限定兼容的浏览器版本，格式{browser:version}，browser可以使用ie,edge,firefox,opera,chrome,safari；version表示版本数值|
+| operator | String | >= | 兼容性比较符，可选>=,<=,== |
+| compare | Function(browser,version) | null | 可自定义兼容性比较函数，返回true或false |
+| action | Function(res) | null | 兼容性校验回调函数，res表示兼容性结果，true或false |
--- src/packages/xdo-browser-filter/index.js
+++ src/packages/xdo-browser-filter/index.js
@@ -4,68 +4,61 @@ function getBrowserInfo(){
     let sys = {};
     let ua = navigator.userAgent.toLowerCase();
     let s;
-    (s = ua.match(/rv:([\d\\.]+)\) like gecko/)) ? sys = {browser: 'ie', ver: s[1]}
-        : (s = ua.match(/msie ([\d\\.]+)/)) ? sys = {browser: 'ie', ver: s[1]}
-            : (s = ua.match(/edge\/([\d\\.]+)/)) ? sys = {browser: 'edge', ver: s[1]}
-                : (s = ua.match(/firefox\/([\d\\.]+)/)) ? sys = {browser: 'firefox', ver: s[1]}
-                    : (s = ua.match(/(?:opera|opr).([\d\\.]+)/)) ? sys = {browser: 'opera', ver: s[1]}
-                        : (s = ua.match(/chrome\/([\d\\.]+)/)) ? sys = {browser: 'chrome', ver: s[1]}
-                            : (s = ua.match(/version\/([\d\\.]+).*safari/)) ? sys = {browser: 'safari', ver: s[1]} : 0
+    (s = ua.match(/rv:([\d\\.]+)\) like gecko/)) ? sys = {browser: 'ie', version: s[1]}
+        : (s = ua.match(/msie ([\d\\.]+)/)) ? sys = {browser: 'ie', version: s[1]}
+            : (s = ua.match(/edge\/([\d\\.]+)/)) ? sys = {browser: 'edge', version: s[1]}
+                : (s = ua.match(/firefox\/([\d\\.]+)/)) ? sys = {browser: 'firefox', version: s[1]}
+                    : (s = ua.match(/(?:opera|opr).([\d\\.]+)/)) ? sys = {browser: 'opera', version: s[1]}
+                        : (s = ua.match(/chrome\/([\d\\.]+)/)) ? sys = {browser: 'chrome', version: s[1]}
+                            : (s = ua.match(/version\/([\d\\.]+).*safari/)) ? sys = {browser: 'safari', version: s[1]} : 0
     return sys;
 }
 
-function createMongolianLayer(options){
-    let mongolianLayer = document.createElement('div');
-    mongolianLayer.className='bf-monlayer';
-    mongolianLayer.innerHTML='<div class="bf-container">' +
-        '<div class="bf-dialog">' +
-        '<div class="bf-imgContainer">' +
-        '<a href="'+options.newVerUrl+'"  class="bf-imgIcon '+options.class+'" download="'+options.title+'"></a>' +
-        '</div>' +
-        '<p class="bf-description">' +
-        options.title+
-        '</p>' +
-        '</div>' +
-        '</div>';
-    document.getElementsByTagName('body')[0].appendChild(mongolianLayer);
-}
-
-function init(options){
-    let _options={
-        filter:{
-        },
-        title:'您的页面似乎有兼容性问题，点击图标下载最新版Chrome！',
-        class:'',
-        dialogType:'full',
-        newVerUrl: 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B8E74F7DE-458E-65DD-EC25-16AB30074375%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe',
-        downloadName:'ChromeSetup.exe'
-    };
-    _options = Object.assign(_options,options)
-    let browserInfo = getBrowserInfo();
-    if(_options.filter[browserInfo.browser]){
-        if(browserInfo.ver){
-            let verByInt = parseInt(browserInfo.ver.substring(0,browserInfo.ver.indexOf('.')));
-            if(!isNaN(verByInt) && verByInt < _options.filter[browserInfo.browser]){
-                switch(_options.dialogType){
-                    case "fullScreen":{
-                        createMongolianLayer(_options);
-                    }break;
-                    case "modal":{
-                        alert('xxxx')
-                    }break;
-                    default:{
-                        createMongolianLayer(_options);
-                    }break;
-                }
+function compare(browserInfo,operator,filter){
+    let uv=filter[browserInfo.browser]
+    if(uv){
+        switch (operator){
+            case "<=":{
+                return browserInfo.version<=uv
+            }break;
+            case "=":{
+                return uv==browserInfo.version
+                break;
+            }
+            case ">=":
+            default:{
+                return browserInfo.version>=uv
             }
         }
     }else{
         return true
     }
+
 }
 
-function xdoBrowserFilter(options) {
-    init(options);
+function validate(option){
+    let _option={
+        filter:{
+        },
+        operator:'>=',
+        compare:compare,
+        action:null,
+    };
+    _option = Object.assign(_option,option)
+    let browserInfo = getBrowserInfo();
+    let validateRes= true;
+    if(browserInfo.browser&&browserInfo.version){
+        if(_option.compare){
+            validateRes = _option.compare(browserInfo,_option.operator,_option.filter)
+        }
+    }
+    if(_option.action){
+        _option.action(validateRes)
+    }
+}
+
+function xdoBrowserFilter(option) {
+    validate(option);
 }
 
 export default xdoBrowserFilter;
